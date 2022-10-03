8d284a6c0b66c58c9d44d180f67f1d7e8c966e68
Ahmad Nurhasan Hilmi <nurhasanhilmi@gmail.com>
2022-10-03 20:39:12 +0700

    Create interface for services. Closes #4

---
+++ pet-clinic-data/src/main/java/com/hilmi/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.hilmi.petclinic.services;
+
+import com.hilmi.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
