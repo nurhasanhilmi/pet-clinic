084f92e548cbddeb9c18ffa6636ce72e23dc15e3
Ahmad Nurhasan Hilmi <nurhasanhilmi@gmail.com>
2022-10-03 20:48:17 +0700

    Add BaseEntity class. Closes #9

--- pet-clinic-data/src/main/java/com/hilmi/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/hilmi/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.hilmi.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/com/hilmi/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/hilmi/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.hilmi.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
