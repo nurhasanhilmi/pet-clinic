62ed98b34043f20f45fdc0f94f9ef30c77a9aa8f
Ahmad Nurhasan Hilmi <nurhasanhilmi@gmail.com>
2022-10-12 21:04:33 +0700

    Implement specific service interface.

--- pet-clinic-data/src/main/java/com/hilmi/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/hilmi/petclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package com.hilmi.petclinic.services.map;
 
 import com.hilmi.petclinic.model.Pet;
-import com.hilmi.petclinic.services.CrudService;
+import com.hilmi.petclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
--- pet-clinic-data/src/main/java/com/hilmi/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/hilmi/petclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package com.hilmi.petclinic.services.map;
 
 import com.hilmi.petclinic.model.Vet;
-import com.hilmi.petclinic.services.CrudService;
+import com.hilmi.petclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
