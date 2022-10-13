03712410c89ae912931916ccaeb4e5c6a11d5265
Ahmad Nurhasan Hilmi <nurhasanhilmi@gmail.com>
2022-10-13 14:44:21 +0700

    Implement id manager to map service. Closes #24

--- pet-clinic-data/src/main/java/com/hilmi/petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/hilmi/petclinic/services/map/AbstractMapService.java
@@ -1,12 +1,11 @@
 package com.hilmi.petclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.hilmi.petclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -16,8 +15,17 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T entity) {
-        return map.put(id, entity);
+    T save(T entity) {
+        if (entity != null) {
+            if (entity.getId() == null) {
+                entity.setId(getNextId());
+            }
+            map.put(entity.getId(), entity);
+        } else {
+            throw new IllegalArgumentException("Object cannot be null.");
+        }
+
+        return entity;
     }
 
     void deleteById(ID id) {
@@ -26,5 +34,15 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T entity) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(entity));
+    }
+
+    private Long getNextId() {
+        long nextId = 1L;
+
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException ignored) {}
+
+        return nextId;
     }
 }
--- pet-clinic-web/src/main/java/com/hilmi/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/hilmi/petclinic/bootstrap/DataLoader.java
@@ -20,13 +20,11 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Ahmad");
         owner1.setLastName("Hilmi");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Nur");
         owner2.setLastName("Hasan");
         ownerService.save(owner2);
@@ -34,13 +32,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Load Owners Data");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Lia");
         vet1.setLastName("Amanda");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Aulia");
         vet2.setLastName("Rahmah");
         vetService.save(vet2);
