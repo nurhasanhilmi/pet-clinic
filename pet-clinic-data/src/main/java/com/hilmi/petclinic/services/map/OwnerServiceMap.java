009c9feb82e1aaaa2fe2457666be54632ef7d720
Ahmad Nurhasan Hilmi <nurhasanhilmi@gmail.com>
2022-10-19 13:15:50 +0700

    Update owner's info and pets bootstrap data. Closes #34

--- pet-clinic-data/src/main/java/com/hilmi/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/hilmi/petclinic/services/map/OwnerServiceMap.java
@@ -2,12 +2,22 @@ package com.hilmi.petclinic.services.map;
 
 import com.hilmi.petclinic.model.Owner;
 import com.hilmi.petclinic.services.OwnerService;
+import com.hilmi.petclinic.services.PetService;
+import com.hilmi.petclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
+
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -25,6 +35,20 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner entity) {
+        if (entity == null)
+            return null;
+
+        if (entity.getPets() != null)
+            entity.getPets().forEach(pet -> {
+                if (pet.getPetType() == null)
+                    throw new RuntimeException("Pet type is required");
+
+                petTypeService.save(pet.getPetType());
+
+                pet.setOwner(entity);
+                petService.save(pet);
+            });
+
         return super.save(entity);
     }
 
--- pet-clinic-web/src/main/java/com/hilmi/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/hilmi/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.hilmi.petclinic.bootstrap;
 
 import com.hilmi.petclinic.model.Owner;
+import com.hilmi.petclinic.model.Pet;
 import com.hilmi.petclinic.model.PetType;
 import com.hilmi.petclinic.model.Vet;
 import com.hilmi.petclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.hilmi.petclinic.services.PetTypeService;
 import com.hilmi.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -33,17 +36,38 @@ public class DataLoader implements CommandLineRunner {
 
         System.out.println("Load Pet Types Data");
 
+
+        Pet hilmisCat = new Pet();
+        hilmisCat.setName("Jerry");
+        hilmisCat.setPetType(cat);
+        hilmisCat.setBirthDate(LocalDate.of(2020, 5, 10));
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Ahmad");
         owner1.setLastName("Hilmi");
+        owner1.setAddress("Puri Lestari");
+        owner1.setCity("Bogor Regency");
+        owner1.setTelephone("088899990000");
+        owner1.getPets().add(hilmisCat);
         ownerService.save(owner1);
+
+
+        Pet hasansDog = new Pet();
+        hasansDog.setName("Diamond");
+        hasansDog.setPetType(dog);
+        hasansDog.setBirthDate(LocalDate.of(2021, 9, 15));
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Nur");
         owner2.setLastName("Hasan");
+        owner2.setAddress("Depok Baru");
+        owner2.setCity("Depok");
+        owner2.setTelephone("081208591230");
+        owner2.getPets().add(hasansDog);
         ownerService.save(owner2);
 
         System.out.println("Load Owners Data");
+
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Lia");
