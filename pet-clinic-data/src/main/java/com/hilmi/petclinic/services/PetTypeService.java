d182ee01e30d1b62e1c114393afbd1c96d8fcba1
Ahmad Nurhasan Hilmi <nurhasanhilmi@gmail.com>
2022-10-19 12:01:06 +0700

    Implement PetType map service. Closes #30

---
+++ pet-clinic-data/src/main/java/com/hilmi/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.hilmi.petclinic.services;
+
+import com.hilmi.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/hilmi/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package com.hilmi.petclinic.services.map;
+
+import com.hilmi.petclinic.model.PetType;
+import com.hilmi.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType entity) {
+        super.delete(entity);
+    }
+
+    @Override
+    public PetType save(PetType entity) {
+        return super.save(entity);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
