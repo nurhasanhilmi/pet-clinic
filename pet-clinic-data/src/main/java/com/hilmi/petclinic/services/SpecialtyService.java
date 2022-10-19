439ee2cb9b8a53fd12c9c29364dcc7057dfeb1b8
Ahmad Nurhasan Hilmi <nurhasanhilmi@gmail.com>
2022-10-19 13:37:47 +0700

    Add Specialty Map Service Impl. Closes #32

---
+++ pet-clinic-data/src/main/java/com/hilmi/petclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package com.hilmi.petclinic.services;
+
+import com.hilmi.petclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/hilmi/petclinic/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,35 @@
+package com.hilmi.petclinic.services.map;
+
+import com.hilmi.petclinic.model.Specialty;
+import com.hilmi.petclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty entity) {
+        super.delete(entity);
+    }
+
+    @Override
+    public Specialty save(Specialty entity) {
+        return super.save(entity);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
