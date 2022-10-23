f5d98c84d0caed609bb61c7fb63e114e8a51b958
Ahmad Nurhasan Hilmi <nurhasanhilmi@gmail.com>
2022-10-23 21:06:51 +0700

    Implement Visit Spring Data JPA Service. Closes #48

---
+++ pet-clinic-data/src/main/java/com/hilmi/petclinic/services/jpa/VisitServiceJpa.java
@@ -0,0 +1,47 @@
+package com.hilmi.petclinic.services.jpa;
+
+import com.hilmi.petclinic.model.Visit;
+import com.hilmi.petclinic.repositories.VisitRepository;
+import com.hilmi.petclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("spring-data-jpa")
+public class VisitServiceJpa implements VisitService {
+    private final VisitRepository visitRepository;
+
+    public VisitServiceJpa(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit entity) {
+        return visitRepository.save(entity);
+    }
+
+    @Override
+    public void delete(Visit entity) {
+        visitRepository.delete(entity);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
+}
