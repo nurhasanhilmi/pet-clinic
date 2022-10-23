9463721a7e68750c7f69d906b1040114471d2790
Ahmad Nurhasan Hilmi <nurhasanhilmi@gmail.com>
2022-10-23 18:56:17 +0700

    Add Spring Data JPA service for Pet. Closes #43

---
+++ pet-clinic-data/src/main/java/com/hilmi/petclinic/services/jpa/PetServiceJpa.java
@@ -0,0 +1,47 @@
+package com.hilmi.petclinic.services.jpa;
+
+import com.hilmi.petclinic.model.Pet;
+import com.hilmi.petclinic.repositories.PetRepository;
+import com.hilmi.petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("spring-data-jpa")
+public class PetServiceJpa implements PetService {
+    private final PetRepository petRepository;
+
+    public PetServiceJpa(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet entity) {
+        return petRepository.save(entity);
+    }
+
+    @Override
+    public void delete(Pet entity) {
+        petRepository.delete(entity);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
