081fcc5cab88de8675cd922839b75344d28d0682
Ahmad Nurhasan Hilmi <nurhasanhilmi@gmail.com>
2022-10-26 19:18:15 +0700

    Add more request mapping to listOwners

--- pet-clinic-web/src/main/java/com/hilmi/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/hilmi/petclinic/controllers/OwnerController.java
@@ -15,7 +15,7 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping("/")
+    @RequestMapping({"", "/", "/index", "/index.html"})
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
