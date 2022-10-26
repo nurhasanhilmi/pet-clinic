ff524a4b7004ae87c017ae20f10a3d48079da13f
Ahmad Nurhasan Hilmi <nurhasanhilmi@gmail.com>
2022-10-26 19:12:41 +0700

    Add Spring MVC test for OwnerController. Closes #53

--- pet-clinic-web/src/main/java/com/hilmi/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/hilmi/petclinic/controllers/OwnerController.java
@@ -15,7 +15,7 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
+    @RequestMapping("/")
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
